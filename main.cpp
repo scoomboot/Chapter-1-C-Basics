eea9345feaad297d936ed4e9a7865283829db756
Eric Moessner <emoessne@gmail.com>
2024-06-05 19:03:30 -0700

    1.2 Comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 int main()
 {
 
-	std::cout << "This is a statement!\n";
+	std::cout << "This is a statement!\n";// This is a single line comment.
+
+	/* 
+		This is a
+		Multi-line
+		comment.
+	
+	*/
 
 	return 0;
 }
