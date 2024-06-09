f4bc1d5dc5fa535c70b09f1d3915a3306fb0db22
Eric Moessner <emoessne@gmail.com>
2024-06-09 09:13:37 -0700

    1.8 Whitespace and basic formatting

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 int main()
 {
-	//1.5
+	//1.7
 
-	int x; // This is an uninitialized vairable, we have not given it a value.
 
-	std::cout << x; // this will give an error "uninitialized local variable"s
+	int my_variable_name;   // conventional (separated by underscores/snake_case)
+	int my_function_name(); // conventional (separated by underscores/snake_case)
+
+	int myVariableName;     // conventional (intercapped/camelCase)
+	int myFunctionName();   // conventional (intercapped/camelCase)
+
+	int my variable name;   // invalid (whitespace not allowed)
+	int my function name(); // invalid (whitespace not allowed)
+
+	int MyVariableName;     // unconventional (should start with lower case letter)
+	int MyFunctionName();   // unconventional (should start with lower case letter)
 
 	return 0;
 }
